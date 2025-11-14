59fcbecf395f5d080b65a90640044d39bc82d45c
thitna8 <thitnadamtew@gmail.com>
2025-11-14 16:02:20 +0300

    refactor: use TypedResults API and implement DTO pattern

--- obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27a0c10c9e7cca06c298c8fb4110ebde0df246b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fda69dbadf669f15bb59e0b318ff112eef54877")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
